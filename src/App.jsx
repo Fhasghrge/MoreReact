cb59745d350e4f15d4bb50c7efa4f51159dea0f4
Fhasghrge <1969533391@qq.com>
2020-06-19 20:50:17 +0800

    Add: use memo in Function Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import React, { Component, PureComponent } from 'react';
+import React, { Component, memo } from 'react';
 import './App.css';
 
-
 /**
- * PureComponent中shouldComponentUpdate只进行浅比较，对于对象不进行深度比较
- * shouldComponentUpdata进行比较时，
- * 如果传入的是内联函数，每次都需要重新渲染，如果传入的是函数名，就不会
- * 函数使用箭头函数可以使得 函数变为类的属性，不同显示绑定函数的this
- * **/
-class Foo extends PureComponent {
-  render() {
-    console.log('Foo render!');
-    return (
-      <>
-        Foo
-      </>
-    )
-  }
-}
+ * use "memo" in Function Component
+ * make the render celebrate
+ */
+const Foo = memo(function Foo(props) {
+  console.log('Foo render!');
+  return <>Foo</>;
+})
 class App extends Component {
   state = {
-    count: 0
-  }
+    count: 0,
+  };
   render() {
     return (
       <div>
-      <button onClick={() => this.setState({count: this.state.count + 1})}>ADD</button>
-      <p>{this.state.count}</p>
-      <Foo name="MIKE"/>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+          ADD
+        </button>
+        <p>{this.state.count}</p>
+        <Foo name="MIKE" />
       </div>
-    )
+    );
   }
 }
 export default App;
