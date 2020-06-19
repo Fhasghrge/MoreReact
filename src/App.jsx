6d4c11c3fd686ab58cd0949c2bbb801e9e0ceaef
Fhasghrge <1969533391@qq.com>
2020-06-19 18:29:44 +0800

    Add: lazy, suspense, error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,36 @@
-import React, { createContext, Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import './App.css';
-const BatteryContext = createContext(90);
-class Left extends Component {
-  static contextType = BatteryContext;
-  render() {
-    return (
-        <h1>battery: {this.context}</h1>
-    )
-  }
-}
-class Middle extends Component {
-  render () {
-    return <Left />;
-  }
-}
+
+const About = lazy(() => import(/* webpackChunkName: "about"*/ './about.jsx'));
+
+// ErrorBoundary
+// ComponentDidCatch
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 80,
-    };
+  state = {
+    hasError: false,
+  };
+  // 捕获边界错误
+  // componentDidCatch() {
+  //   this.setState({
+  //     hasError: true,
+  //   });
+  // }
+  static getDerivedStateFromError() {
+    return {
+      hasError: true
+    }
   }
   render() {
+    if (this.state.hasError) {
+      return <div>Error</div>;
+    }
     return (
-      <BatteryContext.Provider value={this.state.value}>
-        <button
-          onClick={() =>
-            this.setState((pre) => ({
-              value: pre.value - 1,
-            }))
-          }
-        >
-          Press
-        </button>
-        <Middle />
-      </BatteryContext.Provider>
+      <>
+        <Suspense fallback={<div>loading...</div>}>
+          <About />
+        </Suspense>
+      </>
     );
   }
 }
-
 export default App;
