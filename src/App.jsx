4e46e32b137e959cd5b68713439745688da69337
Fhasghrge <1969533391@qq.com>
2020-06-19 17:20:45 +0800

    Add: use contextType replace consumer

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,40 @@
 import React, { createContext, Component } from 'react';
 import './App.css';
-const Battery = createContext();
-const Online = createContext();
-function Left() {
-  return (
-    <Battery.Consumer>
-      {(battery) => (
-        <Online.Consumer>
-          {(online) => (
-            <h1>
-              battery: {battery}, online: {String(online)}
-            </h1>
-          )}
-        </Online.Consumer>
-      )}
-    </Battery.Consumer>
-  );
+const BatteryContext = createContext(90);
+class Left extends Component {
+  static contextType = BatteryContext;
+  render() {
+    return (
+        <h1>battery: {this.context}</h1>
+    )
+  }
 }
-function Middle() {
-  return <Left />;
+class Middle extends Component {
+  render () {
+    return <Left />;
+  }
 }
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       value: 80,
-      online: true,
     };
   }
   render() {
     return (
-      <Battery.Provider value={this.state.value}>
-        <Online.Provider value={this.state.online}>
-          <button
-            onClick={() =>
-              this.setState((pre) => ({
-                value: pre.value - 1,
-              }))
-            }
-          >
-            Press
-          </button>
-          <button
-            onClick={() =>
-              this.setState((pre) => ({
-                online: !pre.online,
-              }))
-            }
-          >
-            Switch
-          </button>
-          <Middle />
-        </Online.Provider>
-      </Battery.Provider>
+      <BatteryContext.Provider value={this.state.value}>
+        <button
+          onClick={() =>
+            this.setState((pre) => ({
+              value: pre.value - 1,
+            }))
+          }
+        >
+          Press
+        </button>
+        <Middle />
+      </BatteryContext.Provider>
     );
   }
 }
