bc57a43a2de6f0cbffa857da1be72b9e3ddbdf78
Fhasghrge <1969533391@qq.com>
2020-06-29 21:31:02 +0800

    UseEffect: 3 style of use

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-const App = (props) => {
-
-  // useState use function paras to lazy init to celebrate
-  const [count, setCount] = useState(() => {
-    console.log('init Count');
-    return props.defaultCount || 0
+function App() {
+  const [count, setCount] = useState(0);
+  const [size, setSize] = useState({
+    width: document.documentElement.clientWidth,
+    height: document.documentElement.clientHeight,
   });
+  const onResize = () => {
+    setSize({
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+    });
+  };
+  useEffect(() => { // 在DidMount DidUpdate
+    document.title = count;
+  });
+  useEffect(() => { // 在DisMount WillUmount
+    window.addEventListener('resize', onResize, false);
+    return () => {
+      window.removeEventListener('resize', onResize, false);
+    };
+  }, []);// 只有数组的每一项都不变，useEffect才不会执行
+
+  useEffect(() => {
+    console.log('effect')
+  }, [size])
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Add</button>
-      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>click</button>
+      ={">"} {count}
+      <div>
+        size: {size.width} x {size.height}
+      </div>
     </div>
   );
 }
+
 export default App;
